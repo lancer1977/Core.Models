1ad6d1683ee9e13be7b3c48acd2c301aabc5c039
chris <lancer1977@gmail.com>
2024-11-01 22:29:56 -0400

    Mark Observable<T> obsolete, add ValueObservable<T>
    - Marked `Observable<T>` as obsolete with `[Obsolete("Use ValueObservable<T> instead for thread safety")]`.
    - Implemented `IDisposable` in `Observable<T>` and added `Dispose` method.
    - Refactored `SetValue` method in `Observable<T>` for readability.
    - Introduced `ValueObservable<T>` class implementing `IObservable<T>` and `IDisposable`.
    - Added thread-safe `SetValue` method in `ValueObservable<T>` using a lock.
    - Added `AsObservable` method in `ValueObservable<T>` to expose observable sequence.
    - Ensured proper disposal of `_subject` in both `Observable<T>` and `ValueObservable<T>`.

--- Models/Observable.cs
+++ Models/Observable.cs
@@ -6,24 +6,28 @@ using System.Reactive.Subjects;
 
 namespace PolyhydraGames.Core.Models;
 
-public class Observable<T> : IObservable<T>
+[Obsolete("Use ValueObservable<T> instead for thread saftey")]
+public class Observable<T> : IObservable<T>, IDisposable where T : class
 {
     private T _value;
 
     public T Value
     {
         get => _value;
-  
+
     }
 
     public bool SetValue(T value)
     {
-        
-            if (EqualityComparer<T>.Default.Equals(_value, value)) return false;
-            _value = value;
-            _subject.OnNext(value);
-            return true;
+        if (EqualityComparer<T>.Default.Equals(_value, value))
+            return false;
+
+
+        _value = value;
+        _subject.OnNext(value);
+        return true;
     }
+
     private readonly Subject<T> _subject = new();
     private readonly IObservable<T> _observable;
     public Observable()
@@ -35,6 +39,47 @@ public class Observable<T> : IObservable<T>
     {
         return _observable.Subscribe(observer);
     }
-
+    public void Dispose()
+    {
+        _subject.Dispose();
+    }
 
 }
+
+public class ValueObservable<T> : IObservable<T>, IDisposable where T : class
+{
+    private T _value = default!; // Initialize with default value
+
+    public T Value => _value;
+
+    private readonly Subject<T> _subject = new();
+    public IObservable<T> AsObservable()
+    {
+        return _subject.AsObservable();
+    }
+
+    public bool SetValue(T value)
+    {
+        if (EqualityComparer<T>.Default.Equals(_value, value))
+            return false;
+
+        lock (_subject) // Use a lock for thread safety
+        {
+            _value = value;
+            _subject.OnNext(value);
+            return true;
+        }
+    }
+
+    public IDisposable Subscribe(IObserver<T> observer)
+    {
+        return _subject.Subscribe(observer);
+    }
+
+    public void Dispose()
+    {
+        _subject.Dispose();
+    }
+}
+
+ 
